9b41c1493d2ba3b83e164a3ca0cb17f84845db36
eddiechou <eddieechou@gmail.com>
2017-02-08 14:58:55 -0800

    Completed stack in functional with shared methods instantiation pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  _.extend(obj, stackMethods);
+  obj.stackSize = 0;
+  obj.storage = {};
+  return obj;
+
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  size: function() {
+    return this.stackSize;
+  },
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+  pop: function() {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      return this.storage[this.stackSize];
+    } else {
+      return null;
+    }
+  }
+};
 
 
