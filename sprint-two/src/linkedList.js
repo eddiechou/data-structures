d18cfe3ab229fadc662c54fa06083115e80ec9d6
eddiechou <eddieechou@gmail.com>
2017-02-08 16:37:06 -0800

    Passed given tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (list.tail) {
+      list.tail.next = node;
+      list.tail = list.tail.next;
+    } else {
+      list.tail = node;
+      list.head = node;
+    }
   };
 
   list.removeHead = function() {
+    var result = list.head;
+    if (list.head !== null) {
+      list.head = list.head.next;
+    }
+    return result.value;
   };
 
   list.contains = function(target) {
+    var node = list.head;
+    while (node !== null && target !== node.value) {
+      node = node.next;
+    }
+    if (node === null) {
+      return false;
+    } else {
+      return true;
+    }
   };
 
   return list;
