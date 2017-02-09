3452ec6efdc860bed24fbcf7020cab7781509453
eddiechou <eddieechou@gmail.com>
2017-02-09 11:42:13 -0800

    2 given tests for Binary Search Tree are nwo passing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,37 @@
 var BinarySearchTree = function(value) {
+  var BST = Object.create(BSTmethods);
+  BST.value = value;
+  BST.left = null;
+  BST.right = null;
+  return BST;
 };
 
+var BSTmethods = {
+  insert: function(value) {
+    var node;
+    if (value < this.value) {
+      if (this.left === null) {
+        node = BinarySearchTree(value);
+        this.left = node;
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        node = BinarySearchTree(value);
+        this.right = node;
+      } else {
+        this.right.insert(value);
+      }
+    }
+  },
+  contains: function() {
+  
+  },
+  depthFirstLog: function() {
+  
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
