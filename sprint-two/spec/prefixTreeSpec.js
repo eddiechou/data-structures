2ec8c0c3f75c79296155a7da368f6b04f252a650
eddiechou <eddieechou@gmail.com>
2017-02-11 12:32:50 -0800

    Completed implementation and tests of Prefic tree

--- sprint-two/spec/prefixTreeSpec.js
+++ sprint-two/spec/prefixTreeSpec.js
@@ -5,8 +5,33 @@ describe('prefixTree', function() {
     prefixTree = new PrefixTree('');
   });
 
-  it('should have methods named', function() {
-    //expect(bloomFilter.add).to.be.a('function');
-  
+  it('should have methods named "addString", "checkString" and "removeString"', function() {
+    expect(prefixTree.addString).to.be.a('function');
+    expect(prefixTree.checkString).to.be.a('function');
+    expect(prefixTree.removeString).to.be.a('function');
   });
+
+  it('should add and check strings to the prefix tree', function() {
+    prefixTree.addString('hello');
+    prefixTree.addString('world');
+    prefixTree.addString('help');
+    expect(prefixTree.checkString('hello')).to.equal(true);
+    expect(prefixTree.checkString('world')).to.equal(true);
+    expect(prefixTree.checkString('help')).to.equal(true);
+    expect(prefixTree.checkString('no')).to.equal(false);
+  });
+
+  it('should return false for strings not in the prefixTree', function() {
+    prefixTree.addString('helpline');
+    prefixTree.addString('world');
+    expect(prefixTree.checkString('help')).to.equal(false);
+  });
+
+  it('should remove a word from the prefic tree', function() {
+    prefixTree.addString('helpline');
+    prefixTree.addString('world');
+    prefixTree.removeString('world');
+    expect(prefixTree.checkString('world')).to.equal(false);
+  });
+
 });
--- sprint-two/src/prefixTree.js
+++ sprint-two/src/prefixTree.js
@@ -2,5 +2,40 @@
 // Each node is a prefixTree
 var PrefixTree = function(str) {
   this.letter = str[0];
+  this.isWord = false;
   this.children = {};
 };
+
+PrefixTree.prototype.addString = function(str) {
+  var node = this;
+  for (var i = 0; i < str.length; i++) {
+    if (!node.children.hasOwnProperty(str[i])) {
+      var newNode = new PrefixTree(str[i]);
+      node.children[str[i]] = newNode;
+    }
+    node = node.children[str[i]];
+  }
+  node.isWord = true;
+};
+
+PrefixTree.prototype.checkString = function(str) {
+  var node = this;
+  for (var i = 0; i < str.length; i++) {
+    if (!node.children.hasOwnProperty(str[i])) {
+      return false;
+    }
+    node = node.children[str[i]];
+  }
+  return node.isWord;
+};
+
+PrefixTree.prototype.removeString = function(str) {
+  var node = this;
+  if (this.checkString(str)) {
+    for (var i = 0; i < str.length; i++) {
+      node = node.children[str[i]];
+    }
+    node.isWord = false;
+  }
+};
+
